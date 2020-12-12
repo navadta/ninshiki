cf5768539e020a644bf49c68b965216bb634bf55
Alan GUERET <alan.gueret@epita.fr>
2020-12-12 20:18:46 +0100

    ADD - style.css and the link between css and gtk

---
+++ gui/include/struct.h
@@ -0,0 +1,29 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#include <gtk/gtk.h>
+#include <images/image.h>
+
+typedef struct ui {
+    GtkWindow *window;
+    GtkButton *image_button;
+    GtkButton *start_button;
+    GtkButton *save_button;
+    GtkButton *about_button;
+    GtkButton *quit_button;
+    GtkButton *rotation_button;
+    GtkButton *training_button;
+    GtkTextView *text_panel;
+    GtkImage *image_panel;
+    gint image_x;
+    gint image_y;
+    GtkScale *rotation_scale;
+} UI;
+
+typedef struct app {
+    UI ui;
+    NETWORK network;
+    IMAGE *image;
+} App;
+
+#endif
